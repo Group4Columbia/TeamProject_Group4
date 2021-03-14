6009da972cb5d5aaa96eda1248064cb2a12ccc52
scottrab <71945998+scottrab@users.noreply.github.com>
2021-03-14 09:56:30 -0400

    Code update
    Adding scatterplots and adding chart elements to models

--- 1-2.R
+++ 1-2.R
@@ -2,16 +2,24 @@ d=read.csv("covid_location_data.csv", header=TRUE)
 d[1,]
 fit= lm(d$new_cases ~ d$new_tests) ##linear regression model with tests predicting cases
 summary(fit) ##                     Ignore Blanks 
-cor(d$new_cases, d$new_tests, use = "complete.obs")  ##correlation between cases and tests is .865
+cor(d$new_cases, d$new_tests, use = "complete.obs")  ##correlation between new cases and tests is .865
 options(scipen=999) ##remove scientific notation
+plot(d$new_cases~ d$new_tests)
+plot(d$new_cases, d$new_tests, main='Regression Between New Cases and New Tests' ,xlab='Cases',ylab='Tests',xlim=c(0,300000),ylim=c(0,2500000), pch=20, col= 'blue')
+abline(lm(new_tests ~ new_cases, data= d), col = 'black')
+
 
 fit2= lm(d$new_deaths ~ d$new_cases + d$new_cases_smoothed + d$new_cases_smoothed_per_million)
 summary(fit2) ##                       Ignore Blanks
 cor(d$new_deaths, d$new_cases, use = "complete.obs") ##correlation between new deaths and new cases is .92
 ##new cases, new cases smoothed and new cases smoothed per million are all statistically significant
+plot(d$new_cases, d$new_deaths, main='Regression Between New Cases and New Deaths' ,xlab='Cases',ylab='Deaths')
+abline(lm(new_deaths ~ new_cases, data= d), col = 'red')
+
 
 fit3 = lm(d$total_vaccinations ~ d$new_tests + d$new_cases)
 summary(fit3)
 cor(d$total_vaccinations, d$new_cases, use = "complete.obs")
 
-plot(d$new_deaths, d$new_cases)
+
+     
